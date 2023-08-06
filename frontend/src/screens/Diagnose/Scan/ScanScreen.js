7a25f1868926af2b6fe217b6c83c4332d56c8cef
Sanjula-Dulshan <sdulshan10@gmail.com>
2023-08-06 16:25:56 +0530

    complete camera view

--- frontend/src/screens/Diagnose/Scan/ScanScreen.js
+++ frontend/src/screens/Diagnose/Scan/ScanScreen.js
@@ -1,4 +1,6 @@
 import { Camera } from "expo-camera";
+import * as FileSystem from "expo-file-system";
+import * as ImagePicker from "expo-image-picker";
 import * as MediaLibrary from "expo-media-library";
 import { useEffect, useRef, useState } from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
@@ -7,6 +9,7 @@ import Button from "./Button";
 export default function ScanScreen() {
   const [hasCameraPermission, setHasCameraPermission] = useState(null);
   const [image, setImage] = useState(null);
+  const [gallery, setGallery] = useState(false);
   const type = Camera.Constants.back;
   const cameraRef = useRef(null);
 
@@ -17,9 +20,74 @@ export default function ScanScreen() {
       setHasCameraPermission(cameraStatus.status === "granted");
     })();
   }, []);
+
   if (hasCameraPermission === false) {
     return <Text>No access to camera</Text>;
   }
+
+  const takePicture = async () => {
+    if (cameraRef) {
+      try {
+        const data = await cameraRef.current.takePictureAsync();
+        setImage(data.uri);
+        setGallery(false);
+      } catch (error) {
+        console.log("error ", error);
+      }
+    }
+  };
+
+  const getGalleryImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+      });
+
+      if (!result.canceled) {
+        setImage(result.assets[0].uri);
+        setGallery(true);
+      }
+    } catch (error) {
+      console.log("error ", error);
+    }
+  };
+
+  const checkImage = async () => {
+    if (image) {
+      try {
+        //Save image to gallery
+        if (!gallery) {
+          await MediaLibrary.createAssetAsync(image);
+        }
+
+        const base64Image = await convertToBase64(image); //Convert image to base64
+        await logBase64(base64Image); //to see in console
+
+        //TODO: Send this base64Image to the YOLO model
+      } catch (error) {
+        console.log("error ", error);
+      }
+    }
+  };
+
+  const convertToBase64 = async (image) => {
+    const parts = image.split(".");
+    const extension = parts[parts.length - 1];
+    const base64Data = await FileSystem.readAsStringAsync(image, {
+      encoding: "base64",
+    });
+    const base64 = `data:image/${extension};base64,${base64Data}`;
+
+    return base64;
+  };
+
+  const logBase64 = async (base64String, chunkSize = 200) => {
+    for (let i = 0; i < 100; i += chunkSize) {
+      const chunk = base64String.slice(i, i + chunkSize);
+      console.log("base64String: ", chunk);
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -33,8 +101,8 @@ export default function ScanScreen() {
       <View>
         {!image ? (
           <View style={styles.buttons}>
-            <Button icon="image" />
-            <Button icon="circle" size={70} />
+            <Button icon="image" onPress={getGalleryImage} />
+            <Button icon="circle" size={70} onPress={takePicture} />
             <Button icon="info-with-circle" />
           </View>
         ) : (
