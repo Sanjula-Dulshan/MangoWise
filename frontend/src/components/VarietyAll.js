7c223a52844dbe936fe8028dc6ce8b464e8ab4c3
Dilsha-thathsarani <dilsha99t@gmail.com>
2023-08-28 20:17:51 +0530

    Forecast ui created

--- frontend/src/components/VarietyAll.js
+++ frontend/src/components/VarietyAll.js
@@ -1,34 +1,32 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 
-
 //import screens
 import MarketHomeScreen from "../screens/Market/Home";
 import VarietyHomeScreen from "../screens/Variety/Home";
-
-
-
+import ForecastScreen from "../screens/Market/Forecast/Market";
 
 const Stack = createNativeStackNavigator();
 
 export default function VarietyAllScreens() {
-    return (
-        <Stack.Navigator>
-        <Stack.Screen
-            name="MarketHomeScreen"
-            component={MarketHomeScreen}
-            options={{ headerShown: false }}
-        />
-        
-      
-        <Stack.Screen
-            name="VarietyHomeScreen"
-            component={VarietyHomeScreen}
-            options={{ headerShown: false }}
-        />
-    
-  
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="ForecastScreen"
+        component={ForecastScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="MarketHomeScreen"
+        component={MarketHomeScreen}
+        options={{ headerShown: false }}
+      />
 
-        </Stack.Navigator>
-    );
-    }
+      <Stack.Screen
+        name="VarietyHomeScreen"
+        component={VarietyHomeScreen}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+}
---
+++ frontend/src/screens/Market/Forecast/Market.js
@@ -0,0 +1,120 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import mangoAnalysis from "../../../../assets/M4.jpg";
+import Header from "../../../components/Common/Header";
+
+const YourNewUI = () => {
+  return (
+    <View style={{ backgroundColor: "#ffff", height: "100%" }}>
+      <Header />
+      <ScrollView vertical showsVerticalScrollIndicator={false}>
+        <View style={styles.container}>
+          {/* Top Image */}
+          <Image
+            source={mangoAnalysis}
+            style={styles.topImage}
+            resizeMode="cover"
+          />
+
+          {/* Text Next to Image */}
+          <View style={styles.textContainer}>
+            <Text style={styles.text}>Fresh Mango Price Forecast</Text>
+          </View>
+
+          {/* Card */}
+          <View style={styles.card}>
+            <Text style={styles.cardText}>
+              This would give you the predicted market price for the current
+              month
+            </Text>
+          </View>
+
+          <View style={styles.textContainer}>
+            <Text style={styles.text}>Fresh Mango Market Price</Text>
+          </View>
+
+          <View style={styles.card}>
+            <View style={styles.tableContainer}>
+              <View style={styles.row}>
+                <Text style={styles.label}>Month</Text>
+                <Text style={styles.label}>Price(Rs)</Text>
+              </View>
+              <View style={styles.row}>
+                <Text style={styles.monthText}>January</Text>
+                <Text style={styles.priceText}>$100</Text>
+              </View>
+            </View>
+          </View>
+
+          {/* Button */}
+          <TouchableOpacity style={styles.buttonContainer}>
+            <Text style={styles.buttonText}>Show the Forecast</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f8f8f8",
+  },
+  topImage: {
+    width: "100%",
+    height: 200, // Adjust the height of the top image
+  },
+  textContainer: {
+    padding: 20,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  card: {
+    backgroundColor: "#fff",
+    margin: 20,
+    padding: 15,
+    borderRadius: 10,
+    elevation: 3,
+  },
+  cardTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  cardText: {
+    fontSize: 16,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 5,
+  },
+  label: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  buttonContainer: {
+    backgroundColor: "#fdc50b",
+    margin: 20,
+    padding: 15,
+    borderRadius: 20,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#000",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
+
+export default YourNewUI;
