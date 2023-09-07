79f57ee7a0b86dd5e0dc5b8dbae0647b36a86cff
Sanjula-Dulshan <sdulshan10@gmail.com>
2023-09-07 09:27:42 +0530

    update const

--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -1,8 +1,8 @@
 //TODO: Change the backend_url to your IP address
 const constants = {
   //backend_url: "http://172.20.10.2:8070", //Dilsha
-  //backend_url: "http://192.168.8.115:8070", //Sanjula  172.20.10.6  192.168.8.115
-  backend_url: "http://192.168.8.125:8070", //Yasantha
+  backend_url: "http://192.168.1.101:8070", //Sanjula  172.20.10.6  192.168.8.115
+  //backend_url: "http://192.168.8.125:8070", //Yasantha
 
   disease_cnn_url:
     "https://us-central1-mangowise-395709.cloudfunctions.net/disease_predict_filterRGB",
