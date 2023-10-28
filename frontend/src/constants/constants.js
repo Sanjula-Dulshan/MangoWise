c66c54212d0b4b9c65a80b712262ed99eb5fad36
Sanjula-Dulshan <sdulshan10@gmail.com>
2023-10-29 00:52:39 +0530

    payment ui improve

--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -1,7 +1,7 @@
 //TODO: Change the backend_url to your IP address
 const constants = {
   // backend_url: "http://192.168.1.3:8070", //Dilsha
-  backend_url: "http://192.168.8.115:8070", //Sanjula  172.20.10.6  192.168.8.115
+  backend_url: "http://192.168.4.11:8070", //Sanjula  172.20.10.6  192.168.8.115
   //backend_url: "http://192.168.8.125:8070", //Yasantha
   // backend_url: "http://192.168.1.246:8070", //Dulanjana
 
